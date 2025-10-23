96cd31f03dfad0ac9382ffaac4fb47b7623c63f7
shun <shun@a.shun>
2025-10-23 18:19:14 +0900

    update

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ if __name__ == '__main__':
     p = Pool(4)
 #    noise_model_names = ["Depolar"]
     noise_model_names = ["Depolar","Dephase","AmplitudeDamping","BitFlip"]
-    algorithm_names = ["Vanilla1 NB", "Vanilla20 NB" ,"LinkSelFiE","Vanilla NB"]
-#    algorithm_names = ["Vanilla NB","LinkSelFiE","Succ. Elim. NB",]
+    algorithm_names = ["Vanilla4 NB", "Vanilla20 NB" ,"Vanilla NB","LinkSelFiE"]
+    #    algorithm_names = ["Vanilla NB","LinkSelFiE","Succ. Elim. NB",]
 
     path_num_list = [2, 3, 4, 5, 6, 7]  # For cost vs path num
-    # path_num_list2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20] # For error vs pathnum
-    path_num_list2 = [2, 4, 6, 8, 10, 12, 14]  # For error vs path num    
+    path_num_list2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20] # For error vs pathnum
     gap_list = [0.05 + 0.005 * i for i in range(1, 8)]
     results = []
     for noise_model in noise_model_names:
-        # results.append(p.apply_async(plot_cost_vs_path_num, args=(path_num_list, algorithm_names, noise_model, 20)))
-        # results.append(p.apply_async(plot_cost_vs_gap, args=(4, gap_list, algorithm_names, noise_model, 20)))
-        # results.append(p.apply_async(plot_error_vs_path_num, args=(path_num_list2, algorithm_names, noise_model, 10)))
-        # results.append(p.apply_async(plot_accuracy_vs_gap,   # ← 追加
-        #     args=(4, gap_list, algorithm_names, noise_model, 20)))
-        results.append(p.apply_async(plot_accuracy_vs_path_num, args=(path_num_list2, algorithm_names, noise_model, 20)))
+        results.append(p.apply_async(plot_cost_vs_path_num, args=(path_num_list, algorithm_names, noise_model, 20)))
+        results.append(p.apply_async(plot_cost_vs_gap, args=(4, gap_list, algorithm_names, noise_model, 20)))
+        results.append(p.apply_async(plot_error_vs_path_num, args=(path_num_list2, algorithm_names, noise_model, 10)))
+        results.append(p.apply_async(plot_accuracy_vs_gap,   # ← 追加
+            args=(4, gap_list, algorithm_names, noise_model, 20)))
+        results.append(p.apply_async(plot_accuracy_vs_path_num, args=(path_num_list, algorithm_names, noise_model, 20)))
         
     p.close()
     p.join()
